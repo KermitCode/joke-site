28ec740883605ee9aae01272bccc47d229e48b51
Kermit <956952515@qq.com>
2020-09-09 09:53:41 +0800

    Add files via upload

---
+++ 数据库sql/xh_link.sql
@@ -0,0 +1,7 @@
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (14, '本站手机版', 'http://joke.04007.cn/wap', 15, '2013-2-17 13:16:38', 1);
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (73, '英历史上的今天', 'http://history.04007.cn/en.php', 1, '2015-4-9 21:23:08', 1);
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (74, '04007CN', 'http://www.04007.cn', 100, '2015-4-9 21:23:23', 1);
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (76, '04007天气', 'http://weather.04007.cn/', 50, '2016-8-2 22:34:59', 1);
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (77, '04007笑话', 'http://joke.04007.cn/', 45, '2016-8-2 22:35:23', 1);
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (78, '青岛房产成交量走势', 'http://house.04007.cn', 111, '2017-5-5 09:46:58', 1);
+INSERT INTO `xh_link` (`lk_id`, `lk_title`, `lk_url`, `lk_hot`, `lk_datetime`, `lk_state`) VALUES (75, '历史上的今天', 'http://history.04007.cn', 15, '2016-8-2 22:33:53', 1);
